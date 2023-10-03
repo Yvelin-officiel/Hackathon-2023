76ea838c767c2f4a602ab738cd4fed528c9988a6
Yvelin-officiel <94917819+Yvelin-officiel@users.noreply.github.com>
2023-10-03 11:57:19 +0200

    debug

--- app/src/main/java/com/example/hackathon/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/hackathon/ui/account/AccountFragment.java
@@ -4,18 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import com.example.hackathon.R;
-import com.example.hackathon.databinding.FragmentAccountBinding;
 
 public class AccountFragment extends Fragment {
-
-    private FragmentAccountBinding binding;
 
     public AccountFragment(){
         // require a empty public constructor
@@ -29,6 +21,5 @@ public class AccountFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
--- app/src/main/java/com/example/hackathon/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hackathon/ui/home/HomeFragment.java
@@ -4,18 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import com.example.hackathon.R;
-import com.example.hackathon.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
-
-    private FragmentHomeBinding binding;
 
     public HomeFragment(){
         // require a empty public constructor
@@ -29,6 +21,5 @@ public class HomeFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
